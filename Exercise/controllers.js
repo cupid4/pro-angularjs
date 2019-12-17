4deab96dbedabc86f2a00352abf339729f0ed1e4
ktkwon <ktkwon@sk.com>
2019-12-17 16:07:35 +0900

    controllers 분리

---
+++ Exercise/controllers.js
@@ -0,0 +1,53 @@
+angular.module('todo').controller('TodoCtrl', ['$scope', function ($scope) {
+    $scope.todo = {
+        title: '요가수련',
+        completed: false,
+        createdAt: Date.now()
+    }
+
+
+    $scope.todos = [
+        {
+            title: '요가수련',
+            completed: false,
+            createdAt: Date.now()
+        },
+        {
+            title: '앵귤러 학습',
+            completed: true,
+            createdAt: Date.now()
+        },
+        {
+            title: 'HTML 학습',
+            completed: false,
+            createdAt: Date.now()
+        }
+    ]
+
+    $scope.removeMyItem = function (todoItem, index) {
+        // var idx = $scope.todos.findIndex(function (item) {
+        //     return item.id === todoItem.id;
+        // })
+        // if(idx > -1) {
+        //     $scope.todos.splice(idx, 1);
+        // }
+
+        $scope.todos.splice(index,  1);
+    }
+
+    // $scope.newTodoTitle = function () {
+    //
+    // }
+
+    $scope.add = function (newTodoTitle) {
+        var newTodo = {
+            title: newTodoTitle,
+            completed: false,
+            createdAt: Date.now()
+        }
+        $scope.todos.push(newTodo);
+
+        $scope.newTodoTitle = "";
+    }
+
+}]);
